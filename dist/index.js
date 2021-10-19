b9dc1e3b37edf11028399df2309ecbb62ac27c4f
liang rongjun <liangrongjun@saicmobility.com>
2021-10-19 12:50:44 +0800

    v1.0.10

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,15 @@
+Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+exports.default = loader;
+exports.raw = void 0;
+
 var path = require("path");
 var xlsx = require('node-xlsx').default;
 var _loaderUtils = require("loader-utils");
+var fs = require("fs");
 
-export default function loader (source) {
+function loader (source) {
     var options = (0, _loaderUtils.getOptions)(this) || {};
     // 解析json
     var data = JSON.parse(source);
@@ -15,8 +22,8 @@ export default function loader (source) {
         excelData.push(["", "",   "",         "",     "", data["url"] , excelPos,  formItems[i]["label"], formItems[i]["type"], ""]);
     }
     var buffer = xlsx.build([{name: "mySheetName", data: excelData}]); 
-    fs.writeFileSync(path.join(__dirname, `./${data.filename}.xlsx`), Buffer.from(buffer));
+    fs.writeFileSync(path.join(process.cwd(), `./test/${data.filename}.xlsx`), Buffer.from(buffer));
     return source;
 }
-
-export const raw = true;
+const raw = true;
+exports.raw = raw;
